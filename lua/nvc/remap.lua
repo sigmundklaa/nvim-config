aec0c8632eaf15a68a827f6913bf9ed142119c6b
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2023-11-05 23:16:56 +0100

    Add: more keybinds

--- lua/nvc/remap.lua
+++ lua/nvc/remap.lua
@@ -1,2 +1,20 @@
 vim.g.mapleader = "\t"
 vim.keymap.set("n", "<leader><Enter>", vim.cmd.Ex)
+
+-- Move block up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Append below line to the back of current
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Half-page jump
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Stay in center when jumping between search terms
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste repeatedly
+vim.keymap.set("x", "<leader>p", [["_dP]])
